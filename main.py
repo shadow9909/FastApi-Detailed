8af4aef73759a0982688e99cafd98147fb31cc7a
Archit Maheshwari <arcmahes@cisco.com>
2022-02-01 15:03:43 +0530

    Pass query, path and body params with validation

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from fastapi import FastAPI, Request, Response
+from enum import Enum
+from router import blog
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello():
+    """[summary]
+    - **id** mandatory path parameter
+    - **id** mandatory path parameter
+
+    Returns:
+        [type]: [description]
+    """
+    return {'detail': 'Hello World'}
+
+
+# class BlogType(str, Enum):
+#     short = 'short'
+#     story = 'story'
+#     howto = 'howto'
+
+
+# @app.post('/blog/{id}', status_code=201, tags=['blog'], summary="Get blog with ID", description="List the blog with the received ID")
+# def blog(id: BlogType, response: Response, limit: Optional[int] = 10):
+#     response.status_code = 200
+#     return {'detail': f"{id} is found"}
+
+app.include_router(blog.router)
+
+
+@app.api_route("/{path_name:path}", methods=["GET"], status_code=404)
+async def catch_all(request: Request, path_name: str):
+    return {"request_method": request.method, "path_name": path_name}
---
+++ router/blog.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Response, Body, Query, Path
+from enum import Enum
+from typing import Optional, List, Dict
+from pydantic import BaseModel
+
+router = APIRouter()
+
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+
+class Image(BaseModel):
+    image: str
+    alias: str
+
+
+class BlogModel(BaseModel):
+    id: BlogType
+    title: str
+    comment: str
+    tags: List[str] = []
+    metadata: Dict[str, str] = {}
+    images: Optional[Image] = None
+
+
+@router.post('/blog/{id}/{comment_id}', status_code=201, tags=['blog'], summary="Get blog with ID", description="List the blog with the received ID")
+def blog(id: BlogType, blog: BlogModel, content: str = Body(..., min_length=10, max_length=50, regex='[a-z\s]*$'),  comment_id: int = Path(None, gt=10, le=15), version: Optional[List[str]] = Query([1.0, 2.0]), limit: Optional[int] = 10):
+    # version: Optional[List[str]] = Query(None)
+    # response.status_code = 200
+    return {'blog': blog}
